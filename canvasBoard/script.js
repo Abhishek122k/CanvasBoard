5f8b7bed9f549eca69f34091217959692b74f14b
AbhishekNaik1112 <142667674+AbhishekNaik1112@users.noreply.github.com>
2023-08-21 17:05:47 +0530

    Add files via upload

---
+++ canvasBoard/script.js
@@ -0,0 +1,48 @@
+var strokew = document.querySelector("#strokew");
+var stroke = document.getElementById("stroke");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var strokec = document.getElementById("colorbox");
+
+var x = 0;
+var y = 0;
+
+var isdrawing = false;
+
+function stopdraw() {
+  isdrawing = false;
+}
+
+function startdraw(e) {
+  x = e.offsetX;
+  y = e.offsetY;
+  isdrawing = true;
+}
+
+function draw(e) {
+  if (isdrawing == true) {
+    const newX = e.offsetX;
+    const newY = e.offsetY;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(newX, newY);
+    ctx.stroke();
+
+    x = newX;
+    y = newY;
+  }
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", startdraw);
+canvas.addEventListener("mouseup", stopdraw);
+
+strokew.addEventListener("input", function fun() {
+  stroke.innerText = strokew.value;
+  ctx.lineWidth = strokew.value;
+});
+
+strokec.addEventListener("input", function fun() {
+  ctx.strokeStyle = strokec.value;
+});
